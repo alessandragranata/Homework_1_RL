e25a2758ef228967a28123f6411a2df86137b537
alessandragranata <aless.granata@studenti.unina.it>
2024-10-30 11:04:57 +0100

    Add files via upload

---
+++ arm_control/arm_control/launch/arm_control.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+import xacro
+from launch.conditions import IfCondition, UnlessCondition
+
+
+ 
+def generate_launch_description():
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'yaml_file_config',
+            default_value=PathJoinSubstitution(
+                [FindPackageShare("arm_control"), "config", "arm_control.yaml"]
+            ),
+            description='YAML configuration file'
+        ),
+    )
+
+
+    joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )  
+
+    position_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["position_controller", "--controller-manager", "/controller_manager"],  
+    )
+    
+    nodes_to_start = [
+        joint_state_broadcaster,
+        position_controller
+    ]
+
+    return LaunchDescription(declared_arguments + nodes_to_start) 
